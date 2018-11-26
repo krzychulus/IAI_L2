bed95835e0b0f6ec7ef51140c2ebeba640680a5b
krzychulus <43789645+krzychulus@users.noreply.github.com>
2018-11-26 14:22:40 +0100

    zadanie poprzednie probA 2
    Z FOLDERAMI JS I CSS

---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+let nav = '<nav><a href="index.html">Strona głowna<a/>'+
+    '<a href="contact.html">Kontakt<a/>' +
+    '<a href="mapa.html">Mapa<a/>' +
+    '<a href="quiz.html">Quiz<a/></nav>';
+
+let navBar = document.querySelector('.navbar');
+
+
+navBar.innerHTML = nav;
